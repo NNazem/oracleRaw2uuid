c42eab7c962ee0a11d8fad4090c02d876838106f
navidnazem <nazem.navid@gmail.com>
2025-07-13 22:14:34 +0200

    simplified the code

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+const EXPECTED_INPUT_LENGHT = 32;
+const BUFFER_BYTE_SIZE = 64;
+
+
 pub fn main() !void {
     const args = std.os.argv;
 
@@ -10,22 +14,17 @@ pub fn main() !void {
 
     var in = std.mem.span(args[1]);
 
-    if (in.len != 32){
-        try std.io.getStdErr().writer().print("The hexadecimal need to be exactly 32 chars long\n", .{});
+    if (in.len != EXPECTED_INPUT_LENGHT){
+        try std.io.getStdErr().writer().print("The hexadecimal need to be exactly {d} chars long\n", .{EXPECTED_INPUT_LENGHT});
         return;
     }
 
-    var buffer : [64]u8 = undefined;
+    var buffer : [BUFFER_BYTE_SIZE]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
     const allocator = fba.allocator();
 
-    // We take the two half and convert it to hex array
-    const first_half = try stringToHex(in[0..16], allocator);
-
-    const second_half = try stringToHex(in[16..in.len], allocator);
-
-    reverse(first_half);
-    reverse(second_half);
+    const first_half = try stringToHexReversed(in[0..EXPECTED_INPUT_LENGHT/2], allocator);
+    const second_half = try stringToHexReversed(in[EXPECTED_INPUT_LENGHT/2..in.len], allocator);
 
     const total_len = first_half.len + second_half.len;
 
@@ -34,7 +33,7 @@ pub fn main() !void {
     @memcpy(bytes_array[0..first_half.len], first_half);
     @memcpy(bytes_array[first_half.len..total_len], second_half);
 
-    const convertedBytes = try hexToString(bytes_array, allocator);
+    const converted_bytes = try hexToString(bytes_array, allocator);
 
     const dash_positions = [_]usize{8, 13, 18, 23};
 
@@ -49,42 +48,27 @@ pub fn main() !void {
             out[j] = '-';
             nextDash += 1;
         } else {
-            out[j]   = convertedBytes[i];
+            out[j]   = converted_bytes[i];
             i += 1;
         }
 
         j += 1;
 
     }
-
-
     
     try std.io.getStdOut().writer().print("{s}\n", .{out});
 }
 
-fn reverse(slice : []u8) void{
-    var i : usize = 0;
-    var j : usize = slice.len - 1;
-
-    while (i < j) : ({
-        i += 1;
-        j -= 1;
-    }) {
-        const tmp = slice[i];
-        slice[i] = slice[j];
-        slice[j] = tmp;
-    }
-}
-
-fn stringToHex (hex : []const u8, allocator : std.mem.Allocator) ![]u8 {
+fn stringToHexReversed (hex : []const u8, allocator : std.mem.Allocator) ![]u8 {
     const byte_len = hex.len / 2;
     const bytes = try allocator.alloc(u8, byte_len);
 
     var i : usize = 0;
 
     while(i < byte_len) : (i += 1){
-        const hi = try std.fmt.charToDigit(hex[i * 2], 16);
-        const lo = try std.fmt.charToDigit(hex[i * 2 + 1], 16);
+        const pos = (byte_len - 1 - i) * 2;
+        const hi = try std.fmt.charToDigit(hex[pos], 16);
+        const lo = try std.fmt.charToDigit(hex[pos + 1], 16);
         bytes[i] = @as(u8, (hi << 4) | lo);
     }
 
